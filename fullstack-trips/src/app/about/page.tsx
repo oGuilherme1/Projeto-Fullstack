c0ede6b2e6afcce4ffb4c785fd7ab2f82f6ac91f
Guilherme Gonçalves <guilhermegoncalves1314@gmail.com>
2023-07-04 17:45:16 -0300

    projeto FS

---
+++ fullstack-trips/src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Trips from './components/trips';
+
+export const metadata = {
+    title: "About",
+}
+
+const Page = () => {
+    return (
+    <div>
+        <Trips />       
+        </div>
+    );
+};
+
+
+export default Page;
